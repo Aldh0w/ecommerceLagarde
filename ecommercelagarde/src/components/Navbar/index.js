e81e501df0af3d7d1e6bc861a804740112ebe5e4
Gonzalo Lagarde <gonzalolaga.gl@gmail.com>
2022-07-25 19:56:37 -0300

    Agregando Navbar

---
+++ ecommercelagarde/src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import './Navbar.css';
+
+export default function Navbar () {
+    return(
+        <nav>
+            <ul className='navbar'>
+                <li>Inicio</li>
+                <li>La empresa</li>
+                <li>Productos</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    );
+}
